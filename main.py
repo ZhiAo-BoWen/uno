5dcabcf31c791c8b666bc2a9ef1e6024e0a9cfee
ZhiAo-BoWen <1913774864@qq.com>
2025-03-24 23:03:46 +0800

    修复封装异常问题

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from big_package.wins import win_login, win_ready
 # 公有变量
 
 if __name__ == '__main__':
+    # pyinstaller封装命令
+    # pyinstaller --add-data 'UNO_Assets/Avatars;UNO_Assets/Avatars' .\main.py
     # 启动登录页
     win_login_ = win_login.login_win()
     win_login_.run()
